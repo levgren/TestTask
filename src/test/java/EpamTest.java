66b0b67610a5901a5441b5f06390279b5dd01521
levgren <jekaobodianskyi@gmail.com>
2019-02-20 22:00:03 +0200

    add @before

--- src/test/java/EpamTest.java
+++ src/test/java/EpamTest.java
@@ -1,35 +1,42 @@
 import IevgenTest.pageObjects.MainPage;
 import IevgenTest.pageObjects.SearchPage;
 import io.qameta.allure.Allure;
+import org.aspectj.lang.annotation.Before;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class EpamTest {
     MainPage mainPage = new MainPage();
-        @Test
-        public void testOne() {
-            String text = mainPage.goTo("https://www.google.com/")
-                        .search("automation")
-                        .openFirstLink()
-                        .getTextFromTitle();
-            Allure.addAttachment("check of Header","Header contains 'automation'");
-            Assert.assertTrue(text.toLowerCase().contains("automation"),"Header contains 'automation'");
-        }
-        @Test
-        public void testTwo() {
-            SearchPage searchPage= mainPage.goTo("https://www.google.com/")
-                                           .search("automation");
 
-            Boolean result = false;
-            for(int i = 0; i<5; i++){
-                if(searchPage.fIndDomain("https://www.testautomationday.com/")){
-                    result = true;
-                    break;
-                }
-                searchPage.pressNextButton();
+    @BeforeMethod
+    public void before(){
+        mainPage.goTo("https://www.google.com/");
+    }
+
+    @Test
+    public void testOne() {
+        String text =mainPage
+                    .search("automation")
+                    .openFirstLink()
+                    .getTextFromTitle();
+        Allure.addAttachment("check of Header","Header contains 'automation'");
+        Assert.assertTrue(text.toLowerCase().contains("automation"),"Header contains 'automation'");
+    }
+    @Test
+    public void testTwo() {
+        SearchPage searchPage= mainPage.search("automation");
+
+        Boolean result = false;
+        for(int i = 0; i<5; i++){
+            if(searchPage.fIndDomain("https://www.testautomationday.com/")){
+                result = true;
+                break;
             }
-            Assert.assertTrue(result,"there is no such link on pagination");
+            searchPage.pressNextButton();
         }
+        Assert.assertTrue(result,"there is no such link on pagination");
+    }
 
     }
 
